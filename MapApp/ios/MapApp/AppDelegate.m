53d4893741315ebe24f905c8a682d3f2738aa7d7
agus_prayogi <agusyogi600@gmail.com>
2020-01-29 14:18:48 +0700

    Tambah Map

--- MapApp/ios/MapApp/AppDelegate.m
+++ MapApp/ios/MapApp/AppDelegate.m
@@ -6,7 +6,6 @@
  */
 
 #import "AppDelegate.h"
-#import Firebase;
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -16,12 +15,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  if ([FIRApp defaultApp] == nil) {
-      [FIRApp configure];
-    }
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"ChatApp"
+                                                   moduleName:@"MapApp"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
